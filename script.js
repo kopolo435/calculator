6c77c3ab2e01f4fda1cc27e51421c319a8597ef4
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-02-02 11:05:32 -0500

    Añade variables para almacenar los botones del html

--- script.js
+++ script.js
@@ -31,3 +31,20 @@ function operate(operator,num1,num2){
     }
 }
 
+const addBtn=document.querySelector('#add');
+const subtractBtn=document.querySelector("#substract");
+const multiplyBtn=document.querySelector("#multiply");
+const divideBtn=document.querySelector("#divide");
+const equalBt=document.querySelector("#equal");
+const clearBtn=document.querySelector("#clear");
+
+const ceroBtn=document.querySelector("#0");
+const oneBtn=document.querySelector("#1");
+const twoBtn=document.querySelector("#2");
+const threeBtn=document.querySelector("3");
+const fourthBtn=document.querySelector("#4");
+const fiveBtn=document.querySelector("#5");
+const sixBtn=document.querySelector("#6");
+const sevenBtn=document.querySelector("#7");
+const eightBtn=document.querySelector("#8");
+const nineBtn=document.querySelector("#9");
