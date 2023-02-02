f63f841ed733ce088361fd51af2e639c765007ff
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-02-02 11:34:24 -0500

    Elimina variables de digitos, añade event listener
    Las varibales individuales fueron eliminadas para ser reemplzadas por
    una nodelist que almacena cada digito. Esto se hizo para poder añadir
    un event listener a cada digito.
    
    El event listener se encarga de añadir el digito presionado al display

--- script.js
+++ script.js
@@ -31,6 +31,10 @@ function operate(operator,num1,num2){
     }
 }
 
+function updateDisplay(digitBtn){
+    calcuDisplay.textContent+=digitBtn.textContent;
+}
+const calcuDisplay=document.querySelector(".display");
 const addBtn=document.querySelector('#add');
 const subtractBtn=document.querySelector("#substract");
 const multiplyBtn=document.querySelector("#multiply");
@@ -38,13 +42,8 @@ const divideBtn=document.querySelector("#divide");
 const equalBt=document.querySelector("#equal");
 const clearBtn=document.querySelector("#clear");
 
-const ceroBtn=document.querySelector("#0");
-const oneBtn=document.querySelector("#1");
-const twoBtn=document.querySelector("#2");
-const threeBtn=document.querySelector("3");
-const fourthBtn=document.querySelector("#4");
-const fiveBtn=document.querySelector("#5");
-const sixBtn=document.querySelector("#6");
-const sevenBtn=document.querySelector("#7");
-const eightBtn=document.querySelector("#8");
-const nineBtn=document.querySelector("#9");
+const digitBtns=document.querySelectorAll(".digitBtn");
+digitBtns.forEach(digitBtn => digitBtn.addEventListener("click",function() 
+{updateDisplay(digitBtn)}));
+
+
