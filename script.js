8a17e13b0e6c2a0b43cd8fb953e714b4e0adf619
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-02-02 12:06:51 -0500

    Reemplaza variables de operadores por nodelist
    Agrega tambien un event listener para agregar al display los operadores

--- script.js
+++ script.js
@@ -33,17 +33,23 @@ function operate(operator,num1,num2){
 
 function updateDisplay(digitBtn){
     calcuDisplay.textContent+=digitBtn.textContent;
+    displayValue=calcuDisplay.textContent;
 }
 const calcuDisplay=document.querySelector(".display");
-const addBtn=document.querySelector('#add');
-const subtractBtn=document.querySelector("#substract");
-const multiplyBtn=document.querySelector("#multiply");
-const divideBtn=document.querySelector("#divide");
 const equalBt=document.querySelector("#equal");
 const clearBtn=document.querySelector("#clear");
 
+let displayValue;
+
 const digitBtns=document.querySelectorAll(".digitBtn");
-digitBtns.forEach(digitBtn => digitBtn.addEventListener("click",function() 
-{updateDisplay(digitBtn)}));
+const operatorBtns=document.querySelectorAll(".operatorBtn");
+
+//Agrega al display la el boton seleccionado
+operatorBtns.forEach(operatorbtn => operatorbtn.addEventListener("click", function(){
+    updateDisplay(operatorbtn)}))
+digitBtns.forEach(digitBtn => digitBtn.addEventListener("click",function() {
+    updateDisplay(digitBtn)}));
+
+
 
 
